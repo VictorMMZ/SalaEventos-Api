20d9e18aeda39259073c4a4476703e2c0d48da68
Victor <vmmzdev@gmail.com>
2026-09-04 02:54:28 +0200

    feat:Hace lógica de login, con el 2FA como condicion final para redireccionar en el front

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,14 +1,57 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller{
-    public function index()
+    public function login(Request $request)
     {
-        $users = User::all();
-        return response()->json($users);
+      $validated = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|string|min:6',
+    ]);
+
+    if (!Auth::attempt($validated)) {
+
+        return response()->json([
+            'message' => 'Las credenciales no son válidas'
+        ], 401);
+
     }
 
+   $request->session()->regenerate();
+    $user = Auth::user();
+
+       if (!$user->totp_enabled) {
+            return response()->json([
+                'message' => 'Es necesario configurar el 2FA',
+                'requires_2fa_setup' => true
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Es necesario introducir el código 2FA',
+            'requires_2fa' => true
+        ]);
+    }
+
+       
+    
+
+      public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+       return response()->json([
+        'message' => 'Sesión cerrada correctamente'
+    ]);
+    }
 }
+
+
+
+
