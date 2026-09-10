ea6faeba0f64dd34f2dafb99b3d096750048c2d8
Victor <vmmzdev@gmail.com>
2026-09-10 04:46:46 +0200

    fix:Rutas difinidas para un funcionamiento logico de las peticiones

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,35 @@ use App\Http\Controllers\TwoFactorController;
 use App\Http\Controllers\ReservasController;
 use App\Http\Controllers\ReservasAdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SalasController;
 
- Route::post('/reservas', [ReservasController::class, 'store']);
- Route::get('/2fa/setup', [TwoFactorController::class, 'setup']);
+Route::middleware('web')->group(function () {
+
+    // Públicas
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/reservas', [ReservasController::class, 'store']);
+
+    Route::get('/2fa/setup', [TwoFactorController::class, 'setup']);
     Route::post('/2fa/verify', [TwoFactorController::class, 'verify']);
-   
-    Route::get('/reservas', [ReservasController::class, 'index']);
-    Route::get('/reservas/{id}', [ReservasController::class, 'show']);
-    Route::put('/reservas/{id}', [ReservasController::class, 'update']);
-    Route::delete('/reservas/{id}', [ReservasController::class, 'destroy']);
-Route::middleware('auth:sanctum')->group(function () {
-    
-});
 
+    // Privadas
+    Route::middleware('auth')->group(function () {
+
+        Route::post('/sala', [SalasController::class, 'store']);
+        Route::get('/sala', [SalasController::class, 'index']);
+        Route::get('/sala/{id}', [SalasController::class, 'show']);
+        Route::put('/sala/{id}', [SalasController::class, 'update']);
+        Route::delete('/sala/{id}', [SalasController::class, 'destroy']);
+
+        Route::get('/reservasadmin', [ReservasAdminController::class, 'index']);
+        Route::put('/adminreservas/{id}', [ReservasAdminController::class, 'update']);
+        Route::delete('/adminreservas/{id}', [ReservasAdminController::class, 'destroy']);
+
+        Route::get('/reservas', [ReservasController::class, 'index']);
+        Route::get('/reservas/{id}', [ReservasController::class, 'show']);
+        Route::put('/reservas/{id}', [ReservasController::class, 'update']);
+        Route::delete('/reservas/{id}', [ReservasController::class, 'destroy']);
+    });
+});
